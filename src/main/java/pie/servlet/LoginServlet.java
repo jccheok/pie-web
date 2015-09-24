6a622fe3ce52e64c447ccf92480dd2594a77d609
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-09-25 03:19:21 +0800

    Add user details to servlet response upon successful login

--- src/main/java/pie/servlet/LoginServlet.java
+++ src/main/java/pie/servlet/LoginServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 
+import pie.User;
 import pie.service.UserService;
 import pie.service.UserService.LoginResult;
 
@@ -19,6 +20,8 @@ import com.google.inject.Singleton;
 @Singleton
 public class LoginServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 483038783219697909L;
+	
 	UserService userService;
 	
 	@Inject
@@ -41,9 +44,24 @@ public class LoginServlet extends HttpServlet {
 		
 		for (LoginResult result : LoginResult.values()) {
 			if (result == loginResult) {
+				
 				responseObject.put("result", result.toString());
 				break;
 			}
+		}
+		
+		if (loginResult == LoginResult.SUCCESS) {
+			
+			User user = userService.getUser(userService.getUserID(userEmail));
+			JSONObject userJSON = new JSONObject();
+			userJSON.put("userID", user.getUserID());
+			userJSON.put("userFirstName", user.getUserFirstName());
+			userJSON.put("userLastName", user.getUserLastName());
+			userJSON.put("userType", user.getUserType().name());
+			userJSON.put("userEmail", user.getUserEmail());
+			userJSON.put("userMobile", user.getUserMobile());
+			
+			responseObject.put("user", userJSON);
 		}
 		
 		PrintWriter out = response.getWriter();
