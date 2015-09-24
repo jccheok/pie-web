4b6b9e18eb3df717e4606729dac6dc4cb9a37a3b
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-09-25 03:21:09 +0800

    Add stubs for User Service test

--- test/pie/service/UserServiceTest.java
+++ test/pie/service/UserServiceTest.java
@@ -1,14 +1,42 @@
 package pie.service;
 
-import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserServiceTest {
-
+	
+	UserService userService;
+	
+	@Before
+	public void setupMocks() throws Exception {
+		
+		userService = new UserService();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsRegisteredUser() {
+		
+	}
+	
+	@Test
+	public void testIsVerifiedUser() {
+		
+	}
+	
+	@Test
+	public void testIsValidUser() {
+		
+	}
+	
+	@Test
+	public void testRegisterUserUser() {
+		
 	}
 
 }
