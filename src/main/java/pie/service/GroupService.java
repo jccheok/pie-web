32ba3b80eb768750fbdd647834e6804c6c462bd3
markteo <mark-543210@hotmail.com>
2015-09-25 11:36:02 +0800

    Add more methods in GroupService

--- src/main/java/pie/service/GroupService.java
+++ src/main/java/pie/service/GroupService.java
@@ -7,6 +7,8 @@ import java.util.Date;
 
 import pie.Group;
 import pie.GroupType;
+import pie.School;
+import pie.Student;
 import pie.Teacher;
 import pie.util.DatabaseConnector;
 
@@ -30,13 +32,17 @@ public class GroupService {
 			if (resultSet.next()) {
 				group = new Group();
 				group.setGroupID(groupID);
-				group.setSchool(SchoolService.getSchool(resultSet.getInt("schoolID")));
-				group.setGroupDescription(resultSet.getString("groupDescription"));
+				group.setSchool(SchoolService.getSchool(resultSet
+						.getInt("schoolID")));
+				group.setGroupDescription(resultSet
+						.getString("groupDescription"));
 				group.setGroupName(resultSet.getString("groupName"));
-				group.setGroupMaxDailyHomeworkMinutes(resultSet.getInt("groupMaxDailyHomeworkMinutes"));
-				group.setGroupType(GroupType.getGroupType(resultSet.getInt("groupTypeID")));
+				group.setGroupMaxDailyHomeworkMinutes(resultSet
+						.getInt("groupMaxDailyHomeworkMinutes"));
+				group.setGroupType(GroupType.getGroupType(resultSet
+						.getInt("groupTypeID")));
 				group.setGroupCode(resultSet.getString("groupCode"));
-				group.setGroupIsOpen(resultSet.getInt("groupIsOpen")==1);
+				group.setGroupIsOpen(resultSet.getInt("groupIsOpen") == 1);
 				group.setGroupLastUpdate(new Date(resultSet.getTimestamp(
 						"groupLastUpdate").getTime()));
 				group.setGroupDateCreated(new Date(resultSet.getTimestamp(
@@ -49,46 +55,67 @@ public class GroupService {
 
 		return group;
 	}
-	
-	public int getTotalHomeworkMinutesToday(int groupID){
-		
+
+	public int getTotalHomeworkMinutesToday(int groupID) {
+
 		int totalMinutes = 0;
-		
-		//Write codes to retrieve total minutes of homework for the day
-		
+
+		// Write codes to retrieve total minutes of homework for the day
+
 		return totalMinutes;
-		
 	}
-	
-	public Teacher getAdministrator(int groupID){
-		
+
+	public Teacher getAdministrator(int groupID) {
+
 		Teacher administrator = null;
-		
-		//Write codes to retrieve Administrator of the group
-		
+
+		// Write codes to retrieve Administrator of the group
+
 		return administrator;
 	}
-	
-	public boolean setAdministrator(int groupID){
-		
+
+	public boolean setAdministrator(int groupID) {
+
 		boolean result = false;
-		
-		//Write codes to set the Administrator for the group
-		
+
+		// Write codes to set the Administrator for the group
+
 		return result;
 	}
-	
-	public Teacher[] getTeacherMembers(int groupID){
-		
+
+	public Teacher[] getTeacherMembers(int groupID) {
+
 		Teacher[] teacherMembers = {};
-		
-		//Write codes to retrieve the Members of Group that are Teachers
-		
+
+		// Write codes to retrieve the Members of Group that are Teachers
+
 		return teacherMembers;
 	}
-	
-	
-	
-	
+
+	public Student[] getStudentMembers(int groupID) {
+		Student[] studentMembers = {};
+
+		// Write codes to retrieve the Members of Group that are Students
+
+		return studentMembers;
+	}
+
+	public boolean addStudentToGroup(int groupID) {
+		boolean addResult = false;
+
+		// Add Student to Group
+
+		return addResult;
+	}
+
+	public int registerGroup(String groupName, School school,
+			String groupDescription, int groupMaxDailyHomeworkMinutes,
+			GroupType groupType, String groupCode, boolean groupIsOpen) {
+		int groupID = -1;
+
+		// Write codes to Register Group
+
+		return groupID;
+	}
 	
 }
