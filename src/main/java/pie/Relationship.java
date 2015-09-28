5687bd8dfdf4130e20e4f659e7abd6999992b18c
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-09-28 11:06:54 +0800

    Initial commit of Relationship

---
+++ src/main/java/pie/Relationship.java
@@ -0,0 +1,24 @@
+package pie;
+
+public class Relationship {
+
+	private int relationshipID;
+	private String relationshipName;
+
+	public int getRelationshipID() {
+		return relationshipID;
+	}
+
+	public void setRelationshipID(int relationshipID) {
+		this.relationshipID = relationshipID;
+	}
+
+	public String getRelationshipName() {
+		return relationshipName;
+	}
+
+	public void setRelationshipName(String relationshipName) {
+		this.relationshipName = relationshipName;
+	}
+
+}
