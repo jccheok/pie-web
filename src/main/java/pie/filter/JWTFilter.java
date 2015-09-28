dbf842d7e8d03f089731863861937de5a02f0c99
jccheok <cheok.jia.chin@gmail.com>
2015-09-28 16:31:19 +0800

    Update init in JWTFilter

--- src/main/java/pie/filter/JWTFilter.java
+++ src/main/java/pie/filter/JWTFilter.java
@@ -22,56 +22,55 @@ import com.auth0.jwt.JWTVerifier;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 import pie.service.AuthService;
 
-@WebFilter(filterName= "jwt-filter", urlPatterns = { "/secured/*" })
+@WebFilter(filterName = "jwt-filter", urlPatterns = { "/secured/*" })
 public class JWTFilter implements Filter {
 	private JWTVerifier jwtVerifier;
 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		jwtVerifier = new JWTVerifier(DatatypeConverter.parseBase64Binary(AuthService.getSecretKey()),
+				System.getenv("iss"));
+	}
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        jwtVerifier = new JWTVerifier(
-          DatatypeConverter.parseBase64Binary(AuthService.getSecretKey()),
-          System.getenv("aud"));
-    }
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String token = getToken((HttpServletRequest) request);
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String token = getToken((HttpServletRequest) request);
+		try {
+			Map<String, Object> decoded = jwtVerifier.verify(token);
+			// Do something with decoded information like UserId
+			chain.doFilter(request, response);
+		} catch (Exception e) {
+			throw new ServletException("Unauthorized: Token validation failed", e);
+		}
+	}
 
-        try {
-            Map<String, Object> decoded = jwtVerifier.verify(token);
-            // Do something with decoded information like UserId
-            chain.doFilter(request, response);
-        } catch (Exception e) {
-            throw new ServletException("Unauthorized: Token validation failed", e);
-        }
-    }
+	private String getToken(HttpServletRequest httpRequest) throws ServletException {
+		String token = null;
+		final String authorizationHeader = httpRequest.getHeader("X-auth");
+		if (authorizationHeader == null) {
+			throw new ServletException("Unauthorized: No Authorization header was found");
+		}
 
-    private String getToken(HttpServletRequest httpRequest) throws ServletException {
-    	String token = null;
-        final String authorizationHeader = httpRequest.getHeader("X-auth");
-        if (authorizationHeader == null) {
-            throw new ServletException("Unauthorized: No Authorization header was found");
-        }
+		String[] parts = authorizationHeader.split(" ");
+		if (parts.length != 2) {
+			throw new ServletException("Unauthorized: Format is Authorization: Bearer [token]");
+		}
 
-        String[] parts = authorizationHeader.split(" ");
-        if (parts.length != 2) {
-            throw new ServletException("Unauthorized: Format is Authorization: Bearer [token]");
-        }
+		String scheme = parts[0];
+		String credentials = parts[1];
 
-        String scheme = parts[0];
-        String credentials = parts[1];
-
-        Pattern pattern = Pattern.compile("^Bearer$", Pattern.CASE_INSENSITIVE);
-        if (pattern.matcher(scheme).matches()) {
-            token = credentials;
-        }
-        return token;
-    }
+		Pattern pattern = Pattern.compile("^Bearer$", Pattern.CASE_INSENSITIVE);
+		if (pattern.matcher(scheme).matches()) {
+			token = credentials;
+		}
+		return token;
+	}
 
 	@Override
 	public void destroy() {
-		
+
 	}
 
 }
