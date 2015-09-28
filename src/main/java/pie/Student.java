fb35bf0731e5fb72420c52d6712740ed5387dc86
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-09-28 11:02:53 +0800

    Add enlistment date as parameter in Student

--- src/main/java/pie/Student.java
+++ src/main/java/pie/Student.java
@@ -1,24 +1,35 @@
 package pie;
 
+import java.util.Date;
+
 public class Student extends User {
 
 	private School school;
 	private String studentCode;
+	private Date studentEnlistmentDate;
 
 	public School getSchool() {
 		return school;
 	}
-	
+
 	public void setSchool(School school) {
 		this.school = school;
 	}
-	
+
 	public String getStudentCode() {
 		return studentCode;
 	}
-	
+
 	public void setStudentCode(String studentCode) {
 		this.studentCode = studentCode;
 	}
-	
+
+	public Date getStudentEnlistmentDate() {
+		return studentEnlistmentDate;
+	}
+
+	public void setStudentEnlistmentDate(Date studentEnlistmentDate) {
+		this.studentEnlistmentDate = studentEnlistmentDate;
+	}
+
 }
