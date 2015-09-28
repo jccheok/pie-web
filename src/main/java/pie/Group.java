c88ffcf90e36adcf763cf45b542a6fbf9585423c
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-09-28 11:04:01 +0800

    Add groupDateDeleted and groupIsValid parameters to Group

--- src/main/java/pie/Group.java
+++ src/main/java/pie/Group.java
@@ -14,88 +14,102 @@ public class Group {
 	private boolean groupIsOpen;
 	private Date groupLastUpdate;
 	private Date groupDateCreated;
-	
+	private Date groupDateDeleted;
+	private boolean groupIsValid;
 	public int getGroupID() {
 		return groupID;
 	}
-	
+
 	public void setGroupID(int groupID) {
 		this.groupID = groupID;
 	}
-	
+
 	public School getSchool() {
 		return school;
 	}
-	
+
 	public void setSchool(School school) {
 		this.school = school;
 	}
-	
+
 	public String getGroupName() {
 		return groupName;
 	}
-	
+
 	public void setGroupName(String groupName) {
 		this.groupName = groupName;
 	}
-	
+
 	public String getGroupDescription() {
 		return groupDescription;
 	}
-	
+
 	public void setGroupDescription(String groupDescription) {
 		this.groupDescription = groupDescription;
 	}
-	
+
 	public int getGroupMaxDailyHomeworkMinutes() {
 		return groupMaxDailyHomeworkMinutes;
 	}
-	
+
 	public void setGroupMaxDailyHomeworkMinutes(int groupMaxDailyHomeworkMinutes) {
 		this.groupMaxDailyHomeworkMinutes = groupMaxDailyHomeworkMinutes;
 	}
-	
+
 	public GroupType getGroupType() {
 		return groupType;
 	}
-	
+
 	public void setGroupType(GroupType groupType) {
 		this.groupType = groupType;
 	}
-	
+
 	public String getGroupCode() {
 		return groupCode;
 	}
-	
+
 	public void setGroupCode(String groupCode) {
 		this.groupCode = groupCode;
 	}
-	
-	public boolean isGroupIsOpen() {
+
+	public boolean groupIsOpen() {
 		return groupIsOpen;
 	}
-	
+
 	public void setGroupIsOpen(boolean groupIsOpen) {
 		this.groupIsOpen = groupIsOpen;
 	}
-	
+
 	public Date getGroupLastUpdate() {
 		return groupLastUpdate;
 	}
-	
+
 	public void setGroupLastUpdate(Date groupLastUpdate) {
 		this.groupLastUpdate = groupLastUpdate;
 	}
-	
+
 	public Date getGroupDateCreated() {
 		return groupDateCreated;
 	}
-	
+
 	public void setGroupDateCreated(Date groupDateCreated) {
 		this.groupDateCreated = groupDateCreated;
 	}
-	
-	
-	
-	
+
+	public Date getGroupDateDeleted() {
+		return groupDateDeleted;
+	}
+
+	public void setGroupDateDeleted(Date groupDateDeleted) {
+		this.groupDateDeleted = groupDateDeleted;
+	}
+
+	public boolean groupIsValid() {
+		return groupIsValid;
+	}
+
+	public void setGroupIsValid(boolean groupIsValid) {
+		this.groupIsValid = groupIsValid;
+	}
+
 }
