599f4fce3ec5536720273c1510611e9f0971dc39
markteo <mark-543210@hotmail.com>
2015-10-01 13:56:37 +0800

    Refactor AddChildServlet to suit new convention

--- src/main/java/pie/servlets/AddChildServlet.java
+++ src/main/java/pie/servlets/AddChildServlet.java
@@ -2,6 +2,7 @@ package pie.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,7 @@ import com.google.inject.Singleton;
 
 import pie.constants.AddChildResult;
 import pie.services.ParentService;
+import pie.utilities.Utilities;
 
 @Singleton
 public class AddChildServlet {
@@ -27,12 +29,24 @@ public class AddChildServlet {
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setContentType("application/json");
-		response.addHeader("Access-Control-Allow-Origin", "*");
+		int parentID = 0;
+		int relationshipID = 0; 
+		String studentCode = null;
+		
+		try {
 
-		int parentID = Integer.parseInt(request.getParameter("parentID"));
-		int relationshipID = Integer.parseInt(request.getParameter("relationshipID"));
-		String studentCode = request.getParameter("studentCode");
+			Map<String, String> requestParameters = Utilities.getParameters(request, "parentID", "relationshipID",
+					"studentCode");
+			parentID = Integer.parseInt(requestParameters.get("parentID"));
+			relationshipID = Integer.parseInt(requestParameters.get("relationshipID"));
+			studentCode = requestParameters.get("studentCode");
+			
+		} catch (Exception e) {
+
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+		}
+
+		
 				
 		AddChildResult addChildResult = parentService.addChild(parentID, relationshipID, studentCode);
 		
