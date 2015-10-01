35ef79a02594f053a70d2b3983410cb97cc990a4
markteo <mark-543210@hotmail.com>
2015-10-01 13:39:42 +0800

    Refactor RegisterGroupServlet to suit new convention

--- src/main/java/pie/servlets/RegisterGroupServlet.java
+++ src/main/java/pie/servlets/RegisterGroupServlet.java
@@ -2,6 +2,7 @@ package pie.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +16,7 @@ import pie.Staff;
 import pie.constants.GroupRegistrationResult;
 import pie.services.GroupService;
 import pie.services.StaffService;
+import pie.utilities.Utilities;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -23,36 +25,52 @@ import com.google.inject.Singleton;
 public class RegisterGroupServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -3878265942477329631L;
-	
+
 	private GroupService groupService;
 	private StaffService teacherService;
-	
+
 	@Inject
 	public RegisterGroupServlet(GroupService groupService, StaffService teacherService) {
 		this.groupService = groupService;
 		this.teacherService = teacherService;
 	}
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		response.setContentType("application/json");
-		response.addHeader("Access-Control-Allow-Origin", "*");
+		String groupName = null;
+		String groupDescription = null;
+		int groupMaxDailyHomeworkMinutes = 0;
+		int groupTypeID = 0;
+		String groupCode = null;
+		int groupOwnerID = 0;
 
-		String groupName = request.getParameter("groupName");
-		String groupDescription = request.getParameter("groupName");
-		int groupMaxDailyHomeworkMinutes = Integer.valueOf(request.getParameter("groupMaxDailyHomeworkMinutes"));
-		GroupType groupType = GroupType.getGroupType(Integer.valueOf(request.getParameter("groupTypeID")));
-		String groupCode = request.getParameter("groupCode");
-		int groupOwnerID = Integer.valueOf(request.getParameter("teacherID"));
+		try {
+
+			Map<String, String> requestParameters = Utilities.getParameters(request, "groupName", "groupDescription",
+					"groupMaxDailyHomeworkMinutes", "groupTypeID", "groupCode", "staffID");
+			groupName = requestParameters.get("groupName");
+			groupDescription = requestParameters.get("groupDescription");
+			groupMaxDailyHomeworkMinutes = Integer.parseInt(requestParameters.get("groupMaxDailyHomeworkMinutes"));
+			groupTypeID = Integer.parseInt(requestParameters.get("groupTypeID"));
+			groupCode = requestParameters.get("groupCode");
+			groupOwnerID = Integer.parseInt(requestParameters.get("staffID"));
+
+		} catch (Exception e) {
+
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+		}
 		
+		GroupType groupType = GroupType.getGroupType(groupTypeID);
+
 		Staff groupOwner = teacherService.getStaff(groupOwnerID);
-		
-		GroupRegistrationResult registrationResult = groupService.registerGroup(groupOwner, groupName, groupDescription, groupMaxDailyHomeworkMinutes, groupType, groupCode);
+
+		GroupRegistrationResult registrationResult = groupService.registerGroup(groupOwner, groupName,
+				groupDescription, groupMaxDailyHomeworkMinutes, groupType, groupCode);
 
 		JSONObject responseObject = new JSONObject();
 		responseObject.put("result", registrationResult.toString());
 		responseObject.put("message", registrationResult.getDefaultMessage());
-		
+
 		PrintWriter out = response.getWriter();
 		out.write(responseObject.toString());
 
