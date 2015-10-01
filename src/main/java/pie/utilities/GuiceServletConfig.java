62946f88f6ab788690925775beaeae989a5b082e
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-10-02 02:04:07 +0800

    Map all available servlets to corresponding serving URIs

--- src/main/java/pie/utilities/GuiceServletConfig.java
+++ src/main/java/pie/utilities/GuiceServletConfig.java
@@ -2,9 +2,21 @@ package pie.utilities;
 
 import pie.filters.AuthFilter;
 import pie.filters.ResponseFilter;
+import pie.servlets.AddChildServlet;
+import pie.servlets.AddChildToGroupServlet;
 import pie.servlets.GenerateCodeServlet;
 import pie.servlets.LoginServlet;
+import pie.servlets.RegisterGroupServlet;
 import pie.servlets.RegisterParentServlet;
+import pie.servlets.RegisterSchoolServlet;
+import pie.servlets.RegisterStaffServlet;
+import pie.servlets.RegisterStudentServlet;
+import pie.servlets.StaffJoinGroupServlet;
+import pie.servlets.StudentJoinGroupServlet;
+import pie.servlets.UpdateGroupServlet;
+import pie.servlets.VerifyUserServlet;
+import pie.servlets.ViewGroupDetailsServlet;
+import pie.servlets.ViewStaffJoinedGroupsServlet;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -26,8 +38,27 @@ public class GuiceServletConfig extends GuiceServletContextListener {
 				
 				// Servlets
 				serve("*/servlets/login").with(LoginServlet.class);
-				serve("*/servlets/registerParent").with(RegisterParentServlet.class);
-				serve("*/servlets/secured/gencode").with(GenerateCodeServlet.class);
+				serve("*/servlets/registerparent").with(RegisterParentServlet.class);
+				serve("*/servlets/registerstudent").with(RegisterStudentServlet.class);
+				serve("*/servlets/registerstaff").with(RegisterStaffServlet.class);
+				serve("*/servlets/verify").with(VerifyUserServlet.class);
+				
+				serve("*/servlets/secured/student/joingroup").with(StudentJoinGroupServlet.class);
+				
+				serve("*/servlets/secured/parent/addchild").with(AddChildServlet.class);
+				serve("*/servlets/secured/parent/addchildtogroup").with(AddChildToGroupServlet.class);
+				
+				serve("*/servlets/secured/staff/gencode").with(GenerateCodeServlet.class);
+				serve("*/servlets/secured/staff/registergroup").with(RegisterGroupServlet.class);
+				serve("*/servlets/secured/staff/joingroup").with(StaffJoinGroupServlet.class);
+				serve("*/servlets/secured/staff/groupdetails").with(ViewGroupDetailsServlet.class);
+				serve("*/servlets/secured/staff/joinedgroups").with(ViewStaffJoinedGroupsServlet.class);
+				
+				serve("*/servlets/secured/staff/group/member/groupmembers").with(ViewGroupDetailsServlet.class);
+				serve("*/servlets/secured/staff/group/admin/updategroup").with(UpdateGroupServlet.class);
+				
+				serve("*/servlets/secured/admin/registerschool").with(RegisterSchoolServlet.class);
+				
 			}
 		});
 	}
