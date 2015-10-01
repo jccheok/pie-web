8075b9fc5eb6522cc9c44e64be0ec242d6ecac2c
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-10-02 02:02:36 +0800

    Add partial blocks to user type authentication check

--- src/main/java/pie/filters/AuthFilter.java
+++ src/main/java/pie/filters/AuthFilter.java
@@ -12,15 +12,16 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import pie.UserType;
 import pie.services.AuthService;
 
 import com.google.inject.Singleton;
 
 @Singleton
 public class AuthFilter implements Filter {
-	
+
 	private AuthService authService;
-	
+
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		authService = new AuthService();
@@ -34,28 +35,31 @@ public class AuthFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
-		
+
 		try {
-			
+
 			String token = getToken((HttpServletRequest) servletRequest);
-			
+
 			try {
-	            Map<String, Object> decoded = authService.parseJWT(token);
-	            
-	            filterChain.doFilter(servletRequest, servletResponse);
-	        } catch (Exception e) {
-	            throw new ServletException("UNAUTHORIZED: UNRECOGNIZED TOKEN", e);
-	        }
-			
+				Map<String, Object> decoded = authService.parseJWT(token);
+
+				UserType userType = UserType.getUserType((Integer) decoded.get("userTypeID"));
+				String URI = ((HttpServletRequest) servletRequest).getRequestURL().toString();
+
+				filterChain.doFilter(servletRequest, servletResponse);
+			} catch (Exception e) {
+				throw new ServletException("UNAUTHORIZED: UNRECOGNIZED TOKEN", e);
+			}
+
 		} catch (Exception e) {
 			((HttpServletResponse) servletResponse).sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
 		}
 	}
 
 	private String getToken(HttpServletRequest servletRequest) throws ServletException {
-		
+
 		final String token = servletRequest.getHeader("X-Auth-Token");
-		
+
 		if (token == null || token.equals("")) {
 			throw new ServletException("UNAUTHORIZED: NO TOKEN HEADER FOUND IN HEADER");
 		}
