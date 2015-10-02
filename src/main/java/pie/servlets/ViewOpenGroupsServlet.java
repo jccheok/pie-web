ef141b6571501e4337412d6c44955e58f6a85daf
markteo <mark-543210@hotmail.com>
2015-10-02 13:19:56 +0800

    Initial commit of ViewOpenGroupsServlet

---
+++ src/main/java/pie/servlets/ViewOpenGroupsServlet.java
@@ -0,0 +1,86 @@
+package pie.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import pie.Group;
+import pie.Staff;
+import pie.services.GroupService;
+import pie.services.SchoolService;
+import pie.services.StaffService;
+import pie.utilities.Utilities;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class ViewOpenGroupsServlet {
+	GroupService groupService;
+	StaffService staffService;
+	SchoolService schoolService;
+
+	@Inject
+	public ViewOpenGroupsServlet(GroupService groupService, SchoolService schoolService, StaffService staffService) {
+		this.groupService = groupService;
+		this.staffService = staffService;
+		this.schoolService = schoolService;
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int staffID = 0;
+		
+		try {
+		
+			Map<String, String> requestParameters = Utilities.getParameters(request, "staffID");
+			staffID = Integer.parseInt(requestParameters.get("staffID"));
+			
+		} catch (Exception e) {
+			
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+			
+		}
+		
+		staffService = new StaffService();
+		int schoolID = staffService.getStaff(staffID).getSchool().getSchoolID();
+		schoolService = new SchoolService();
+		Group[] groups = schoolService.getSchoolOpenValidGroups(schoolID);
+		
+		JSONObject responseObject = new JSONObject();
+		JSONArray groupList = new JSONArray();
+
+		
+		for (Group group : groups) {
+			HashMap<String, String> currGroup = new HashMap<String, String>();
+			currGroup.put("groupName", group.getGroupName());
+			currGroup.put("groupDescription", group.getGroupDescription());
+			currGroup.put("groupMemberCount", Integer.toString(groupService.getMemberCount(group.getGroupID())));
+		
+			JSONArray adminList = new JSONArray();
+			
+			Staff[] adminStaff = groupService.getGroupAdministrators(group.getGroupID());
+			
+			for (Staff staff : adminStaff) {
+			
+				HashMap<String, String> staffAdmin = new HashMap<String, String>();
+				staffAdmin.put("adminName", staff.getUserFullName());
+				adminList.put(staffAdmin);
+				
+			}
+			
+			currGroup.put("groupAdministrators", adminList.toString());
+			groupList.put(currGroup);
+		}
+		responseObject.put("groupList", groupList);
+		PrintWriter out = response.getWriter();
+		out.write(responseObject.toString());
+	}
+}
