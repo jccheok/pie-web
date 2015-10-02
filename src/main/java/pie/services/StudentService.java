dc6361398e94bf119c1d2b066f0e5180873ff7da
markteo <mark-543210@hotmail.com>
2015-10-02 14:46:47 +0800

    Add enlistStudentToGroups to StudentService

--- src/main/java/pie/services/StudentService.java
+++ src/main/java/pie/services/StudentService.java
@@ -4,7 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import pie.Group;
 import pie.School;
@@ -19,7 +23,7 @@ import pie.utilities.Utilities;
 public class StudentService {
 
 	public boolean isAvailableStudentCode(String studentCode) {
-		
+
 		boolean isAvailable = false;
 
 		try {
@@ -43,7 +47,7 @@ public class StudentService {
 	}
 
 	public boolean isRegisteredStudent(String studentCode) {
-		
+
 		boolean isRegistered = false;
 
 		try {
@@ -72,7 +76,7 @@ public class StudentService {
 	}
 
 	public boolean isMember(Student student, Group group) {
-		
+
 		boolean isMember = false;
 
 		try {
@@ -193,7 +197,7 @@ public class StudentService {
 
 		if (userService.isRegisteredUser(userEmail)) {
 			registrationResult = UserRegistrationResult.EMAIL_TAKEN;
-		} else if (isAvailableStudentCode(studentCode)){
+		} else if (isAvailableStudentCode(studentCode)) {
 			registrationResult = UserRegistrationResult.INVALID_STUDENT_CODE;
 		} else if (isRegisteredStudent(studentCode)) {
 			registrationResult = UserRegistrationResult.STUDENT_REGISTERED;
@@ -227,7 +231,7 @@ public class StudentService {
 		JoinGroupResult joinGroupResult = JoinGroupResult.SUCCESS;
 
 		Group group = groupService.getGroup(groupID);
-		
+
 		if (group == null || !group.groupIsValid()) {
 			joinGroupResult = JoinGroupResult.INVALID_GROUP;
 		} else if (!group.groupIsOpen()) {
@@ -240,24 +244,23 @@ public class StudentService {
 			int nextStudentIndexNumber = groupService.getNextStudentIndexNumber(groupID);
 			groupService.addStudentToGroup(groupID, studentID, nextStudentIndexNumber);
 		}
-		
+
 		return joinGroupResult;
 	}
-	
+
 	public String generateStudentCode() {
-		
+
 		String newStudentCode = Utilities.generateString(5);
 		while (!isAvailableStudentCode(newStudentCode)) {
 			newStudentCode = Utilities.generateString(5);
 		}
-		
+
 		return newStudentCode;
 	}
-	
-	
-	public int getStudentID(String studentCode){
+
+	public int getStudentID(String studentCode) {
 		int studentID = -1;
-		
+
 		try {
 
 			Connection conn = DatabaseConnector.getConnection();
@@ -271,9 +274,9 @@ public class StudentService {
 			resultSet = pst.executeQuery();
 
 			if (resultSet.next()) {
-				
+
 				studentID = resultSet.getInt("studentID");
-				
+
 			}
 
 			conn.close();
@@ -281,8 +284,48 @@ public class StudentService {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 		return studentID;
 	}
 
+	public boolean enlistStudentToGroups(JSONObject studentObject, int schoolID, int groupID){
+		boolean result = false;
+
+		String userFirstName = null;
+		String userLastName = null;
+		String studentCode = null;
+		int studentGroupIndexNumber = 0;
+		
+		try{
+			
+			JSONArray studentList = (JSONArray) studentObject.get("students");
+			ArrayList<JSONArray> tempArrayStudentDetails = new ArrayList<JSONArray>();
+			
+			for(int i = 0; i < studentList.length(); i ++){
+				JSONArray student = (JSONArray) studentList.get(i);
+				tempArrayStudentDetails.add(student);
+			}
+			
+			for(int x = 0; x < tempArrayStudentDetails.size(); x ++){
+				userFirstName = tempArrayStudentDetails.get(x).getJSONObject(0).getString("studentFirstName");
+				userLastName = tempArrayStudentDetails.get(x).getJSONObject(0).getString("studentLastName");
+				studentCode = tempArrayStudentDetails.get(x).getJSONObject(0).getString("studentCode");
+				studentGroupIndexNumber = Integer.parseInt(tempArrayStudentDetails.get(x).getJSONObject(0).getString("studentGroupIndexNumber"));
+				
+				boolean enlistResult = enlistStudent(userFirstName, userLastName, studentCode, schoolID, groupID, studentGroupIndexNumber);
+				
+				if(enlistResult){
+					continue;
+				}else{
+					return result;
+				}
+			}
+			result = true;
+			
+		}catch (Exception e){
+			System.out.println(e);
+		}
+		
+		return result;
+	}
 }
