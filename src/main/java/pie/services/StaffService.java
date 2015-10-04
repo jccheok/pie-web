ce9567b87f708c37b33b793db848e5d5aca043f5
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-10-04 15:56:06 +0800

    Fix syntax error in SQL statement

--- src/main/java/pie/services/StaffService.java
+++ src/main/java/pie/services/StaffService.java
@@ -201,7 +201,7 @@ public class StaffService {
 				PreparedStatement pst = null;
 				ResultSet resultSet = null;
 
-				String sql = "INSERT INTO `User` (userTypeID, userFirstName, userLastName, userEmail, userPassword, userMobile) VALUES (?, ?, ?, ?, ?, ?, ?)";
+				String sql = "INSERT INTO `User` (userTypeID, userFirstName, userLastName, userEmail, userPassword, userMobile) VALUES (?, ?, ?, ?, ?, ?)";
 				pst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 				pst.setInt(1, UserType.STAFF.getUserTypeID());
 				pst.setString(2, userFirstName);
