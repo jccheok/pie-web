4243d1615f27376df54b96e32529b8ab50f7729d
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-10-04 12:21:40 +0800

    Update mail address

--- src/main/java/pie/services/EmailService.java
+++ src/main/java/pie/services/EmailService.java
@@ -55,7 +55,7 @@ public class EmailService {
 	private Message prepareEmailMessage(String emailSubject, String emailContent, String[] emailRecipients, Session session) throws AddressException, MessagingException {
 		
 		Message emailMessage = new MimeMessage(session);
-		emailMessage.setFrom(new InternetAddress("PIE - Partners In Education"));
+		emailMessage.setFrom(new InternetAddress("PIE - Partners In Education <noreply@pie.com.sg>"));
 		for (String email : emailRecipients) {
 			emailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
 		}
