2ab042d82a4e401324055fb0d3701b4ce743e608
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-10-04 13:49:53 +0800

    Change successful registration message

--- src/main/java/pie/constants/UserRegistrationResult.java
+++ src/main/java/pie/constants/UserRegistrationResult.java
@@ -2,7 +2,7 @@ package pie.constants;
 
 public enum UserRegistrationResult {
 
-	SUCCESS("Registration Successful! Please follow the link we've sent to your email to verify your account."),
+	SUCCESS("Please follow the link we've sent to your email to verify your account."),
 	EMAIL_TAKEN("The email you have entered is already taken!"),
 	INVALID_SCHOOL_CODE("The school code you've entered is unrecognized!"),
 	INVALID_STUDENT_CODE("The student code you've entered is unrecognized or is already registered!"), 
