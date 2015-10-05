021c5cf2cbdd6ce6de5ba64fccb1ac7037d7cbba
clarencecastillo <clarence.castillo_33@yahoo.com>
2015-10-05 10:06:32 +0800

    Comment authentication filter for debug and testing

--- src/main/java/pie/filters/AuthFilter.java
+++ src/main/java/pie/filters/AuthFilter.java
@@ -52,66 +52,68 @@ public class AuthFilter implements Filter {
 		HttpServletRequest servletRequest = (HttpServletRequest) request;
 		HttpServletResponse servletResponse = (HttpServletResponse) response;
 
-		try {
-
-			String token = getToken(servletRequest);
-
-			try {
-				Map<String, Object> decoded = authService.parseJWT(token);
-
-				int userID = ((Integer) decoded.get("userID")).intValue();
-				UserType userType = UserType.getUserType(((Integer) decoded.get("userTypeID")).intValue());
-				String requestURI = (servletRequest).getRequestURL().toString();
-
-				String accessType = requestURI.split("/")[5].toUpperCase();
-				
-				if (userType == UserType.ADMIN || userType.toString().equals(accessType)) {
-
-					if (userType == UserType.STAFF && requestURI.split("/")[6].equals("group")) {
-
-						String memberType = requestURI.split("/")[7];
-						int groupID = 0;
-
-						try {
-							Map<String, String> requestParameters = Utilities.getParameters(servletRequest, "groupID");
-							groupID = Integer.parseInt(requestParameters.get("groupID"));
-						} catch (Exception e) {
-
-							servletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
-							return;
-						}
-
-						if (staffService.isMember(userID, groupID)) {
-							StaffRole staffRole = staffService.getStaffRole(userID, groupID);
-
-							if (memberType.equals("owner") && !staffRole.staffRoleIsOwner()) {
-								servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
-								return;
-							} else if (memberType.equals("admin") && (!staffRole.staffRoleIsAdmin() || !staffRole.staffRoleIsOwner())) {
-								servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
-								return;
-							}
-						} else {
-							servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
-							return;
-						}
-					}
-
-					filterChain.doFilter(servletRequest, servletResponse);
-
-				} else {
-
-					servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
-					return;
-				}
-			} catch (Exception e) {
-				throw new ServletException("UNAUTHORIZED: UNRECOGNIZED TOKEN", e);
-			}
-
-		} catch (Exception e) {
-			servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
-			return;
-		}
+//		try {
+//
+//			String token = getToken(servletRequest);
+//
+//			try {
+//				Map<String, Object> decoded = authService.parseJWT(token);
+//
+//				int userID = ((Integer) decoded.get("userID")).intValue();
+//				UserType userType = UserType.getUserType(((Integer) decoded.get("userTypeID")).intValue());
+//				String requestURI = (servletRequest).getRequestURL().toString();
+//
+//				String accessType = requestURI.split("/")[5].toUpperCase();
+//				
+//				if (userType == UserType.ADMIN || userType.toString().equals(accessType)) {
+//
+//					if (userType == UserType.STAFF && requestURI.split("/")[6].equals("group")) {
+//
+//						String memberType = requestURI.split("/")[7];
+//						int groupID = 0;
+//
+//						try {
+//							Map<String, String> requestParameters = Utilities.getParameters(servletRequest, "groupID");
+//							groupID = Integer.parseInt(requestParameters.get("groupID"));
+//						} catch (Exception e) {
+//
+//							servletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+//							return;
+//						}
+//
+//						if (staffService.isMember(userID, groupID)) {
+//							StaffRole staffRole = staffService.getStaffRole(userID, groupID);
+//
+//							if (memberType.equals("owner") && !staffRole.staffRoleIsOwner()) {
+//								servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
+//								return;
+//							} else if (memberType.equals("admin") && (!staffRole.staffRoleIsAdmin() || !staffRole.staffRoleIsOwner())) {
+//								servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
+//								return;
+//							}
+//						} else {
+//							servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
+//							return;
+//						}
+//					}
+//
+//					filterChain.doFilter(servletRequest, servletResponse);
+//
+//				} else {
+//
+//					servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, UNAUTHORIZED_MESSAGE);
+//					return;
+//				}
+//			} catch (Exception e) {
+//				throw new ServletException("UNAUTHORIZED: UNRECOGNIZED TOKEN", e);
+//			}
+//
+//		} catch (Exception e) {
+//			servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+//			return;
+//		}
+		
+		filterChain.doFilter(servletRequest, servletResponse);
 	}
 
 	private String getToken(HttpServletRequest servletRequest) throws ServletException {
