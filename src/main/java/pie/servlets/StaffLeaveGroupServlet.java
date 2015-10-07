7c74a1a33690006e0f531d4961eac086d20b527f
markteo <mark-543210@hotmail.com>
2015-10-07 22:28:50 +0800

    Add Singleton and serialVersionUID to StaffLeaveGroupServlet

--- src/main/java/pie/servlets/StaffLeaveGroupServlet.java
+++ src/main/java/pie/servlets/StaffLeaveGroupServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,9 +17,12 @@ import pie.services.StaffService;
 import pie.utilities.Utilities;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
-public class StaffLeaveGroupServlet {
-	private static final long serialVersionUID = 5666243950594572419L;
+@Singleton
+public class StaffLeaveGroupServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1350818991273599535L;
 
 	StaffService staffService;
 	GroupService groupService;
