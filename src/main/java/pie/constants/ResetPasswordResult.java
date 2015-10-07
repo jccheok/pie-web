d4c0ae5680dfe7d42d604b8ea1d3cd39cc299647
markteo <mark-543210@hotmail.com>
2015-10-07 17:02:30 +0800

    Add one more enum to ResetPasswordResult

--- src/main/java/pie/constants/ResetPasswordResult.java
+++ src/main/java/pie/constants/ResetPasswordResult.java
@@ -3,7 +3,8 @@ package pie.constants;
 public enum ResetPasswordResult {
 	
 	SUCCESS("Password has been reset. Please check your email."), 
-	INVALID_ANSWER("The answer you have entered is wrong!");
+	INVALID_ANSWER("The answer you have entered is wrong!"),
+	RESET_FAILED("The password failed to reset");
 
 	private String defaultMessage;
 
