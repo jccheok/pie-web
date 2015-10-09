50ad9fbec2e932f2c91de27b6148ce910329f193
jccheok <cheok.jia.chin@gmail.com>
2015-10-09 17:03:29 +0800

    Refactor Homework parameters in GetAllDraftNoteServlet

--- src/main/java/pie/servlets/GetAllDraftNoteServlet.java
+++ src/main/java/pie/servlets/GetAllDraftNoteServlet.java
@@ -20,37 +20,37 @@ import pie.services.NoteService;
 import pie.utilities.Utilities;
 
 @Singleton
-public class GetAllDraftNoteServlet extends HttpServlet{
+public class GetAllDraftNoteServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -5614424479603279767L;
-	
+
 	NoteService noteService;
-	
+
 	@Inject
 	public GetAllDraftNoteServlet(NoteService noteService) {
 		this.noteService = noteService;
 	}
-	
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		int staffID = 0;
 		Note[] noteDrafts = null;
-		
+
 		try {
-		
+
 			Map<String, String> requestParameters = Utilities.getParameters(request, "staffID");
 
 			staffID = Integer.parseInt(requestParameters.get("staffID"));
-			
+
 		} catch (Exception e) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
 			return;
 		}
-		
+
 		noteDrafts = noteService.getNoteDrafts(staffID);
 		JSONObject responseObject = new JSONObject();
 		JSONArray noteList = new JSONArray();
-		if(noteDrafts != null) {
+		if (noteDrafts != null) {
 			for (Note note : noteDrafts) {
 				JSONObject noteObject = new JSONObject();
 				noteObject.put("noteID", note.getNoteID());
@@ -60,11 +60,11 @@ public class GetAllDraftNoteServlet extends HttpServlet{
 				noteObject.put("noteResponseQuestionID", note.getNoteQuestionID().getResponseQuestionID());
 				noteList.put(noteObject);
 			}
-		} 
+		}
 		responseObject.put("draftNote", noteList);
 
 		PrintWriter out = response.getWriter();
 		out.write(responseObject.toString());
-		
+
 	}
 }
