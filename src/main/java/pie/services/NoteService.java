3670be4a2ed5ad58dc6f3d1d1bb21c2aa58da354
markteo <mark-543210@hotmail.com>
2015-10-09 14:46:10 +0800

    Refactor deleteDraftNote method in NoteService

--- src/main/java/pie/services/NoteService.java
+++ src/main/java/pie/services/NoteService.java
@@ -383,21 +383,15 @@ public class NoteService {
 		try {
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
-			String sql = "DELETE FROM `GroupNote` WHERE noteID = ?";
-
+		
+			String sql = "DELETE FROM `Note` WHERE noteID = ? AND noteIsDraft = ?";
 			pst = conn.prepareStatement(sql);
 			pst.setInt(1, noteID);
+			pst.setInt(2, 1);
 			
-			if(pst.executeUpdate() == 0){
-				sql = "DELETE FROM `Note` WHERE noteID = ? AND noteIsDraft = ?";
-				pst = conn.prepareStatement(sql);
-				pst.setInt(1, noteID);
-				pst.setInt(2, 1);
-				
-				pst.executeUpdate();
-				
-				deleteNoteResult = true;
-			}
+			pst.executeUpdate();
+			
+			deleteNoteResult = true;
 			
 			conn.close();
 
