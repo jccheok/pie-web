0be4cbaa997edda68cb3b723f36ba6fecbb64d95
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-13 14:27:29 +0800

    Add temp log to UploadAttachmentServlet

--- src/main/java/pie/servlets/UploadAttachmentServlet.java
+++ src/main/java/pie/servlets/UploadAttachmentServlet.java
@@ -34,20 +34,29 @@ public class UploadAttachmentServlet extends HttpServlet {
 
 
 		boolean isUploaded = false;
+		int log = 0;
 		
 		PrintWriter out = response.getWriter();
 		for (Part part : request.getParts()) {
+			log = 1;
 			InputStream is = request.getPart(part.getName()).getInputStream();
+			log = 2;
 			String fileName = attachmentService.getFileName(part);
+			log = 3;
 			FileOutputStream os = new FileOutputStream(System.getenv("OPENSHIFT_DATA_DIR") + fileName);
+			log = 4;
 			byte[] bytes = new byte[BUFFER_LENGTH];
+			log = 5;
 			int read = 0;
+			log = 6;
 			while ((read = is.read(bytes, 0, BUFFER_LENGTH)) != -1) {
 				os.write(bytes, 0, read);
 			}
+			log = 7;
 			os.flush();
 			is.close();
 			os.close();
+			log = 8;
 			out.println(fileName + " was uploaded to " + System.getenv("OPENSHIFT_DATA_DIR"));
 			isUploaded = true;
 		}
@@ -55,7 +64,7 @@ public class UploadAttachmentServlet extends HttpServlet {
 		if(isUploaded) {
 			out.println("Success");
 		} else {
-			out.println("Failed");
+			out.println("Failed" + log);
 		}
 		
 	}
