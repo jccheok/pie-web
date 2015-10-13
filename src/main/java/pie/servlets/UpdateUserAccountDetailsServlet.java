e5003452a952f39af4987ff7ddd6b797137667d4
markteo <mark-543210@hotmail.com>
2015-10-13 16:26:57 +0800

    Some changes to UpdateUserAccountDetailsServlet

--- src/main/java/pie/servlets/UpdateUserAccountDetailsServlet.java
+++ src/main/java/pie/servlets/UpdateUserAccountDetailsServlet.java
@@ -44,7 +44,7 @@ public class UpdateUserAccountDetailsServlet extends HttpServlet{
 		
 		try {
 			
-			Map<String, String> requestParameters = Utilities.getParameters(request, "userID", "userFirstName", "userLastName", "userMobile", "securityQuestionID", "securityQuestionAnswer");
+			Map<String, String> requestParameters = Utilities.getParameters(request, "userID", "userFirstName", "userLastName", "userMobile", "securityQuestionID", "securityQuestionAnswer", "addressStreet", "addressPostalCode", "cityID");
 
 			userID = Integer.parseInt(requestParameters.get("userID"));
 			userFirstName = requestParameters.get("userFirstName");
@@ -52,7 +52,7 @@ public class UpdateUserAccountDetailsServlet extends HttpServlet{
 			userMobile = requestParameters.get("userMobile");
 			securityQuestionID = Integer.parseInt(requestParameters.get("securityQuestionID"));
 			securityQuestionAnswer = requestParameters.get("securityQuestionAnswer");
-			if(requestParameters.containsKey("addressStreet")){
+			if(requestParameters.containsValue("addressPostalCode")){
 				addressStreet = requestParameters.get("addressStreet");
 				addressPostalCode = requestParameters.get("addressPostalCode");
 				cityID = Integer.parseInt(requestParameters.get("cityID"));
