b8a48043bbecb6aa0c749d9efe06870cb549e177
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-13 11:54:18 +0800

    Add getFileName method to AttachmentService Create UploadAttachmentServlet

--- src/main/java/pie/services/AttachmentService.java
+++ src/main/java/pie/services/AttachmentService.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.servlet.http.Part;
+
 import pie.Attachment;
 import pie.AttachmentType;
 import pie.utilities.DatabaseConnector;
@@ -12,49 +14,49 @@ import pie.utilities.DatabaseConnector;
 public class AttachmentService {
 
 	public Attachment getAttachment(int attachmentID) {
-		
+
 		Attachment attachment = null;
-		
+
 		try {
-			
+
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 			ResultSet resultSet = null;
-			
+
 			String sql = "SELECT * FROM `Attachment` WHERE attachmentID = ?";
 			pst = conn.prepareStatement(sql);
 			pst.setInt(1, attachmentID);
-			
+
 			resultSet = pst.executeQuery();
-			
+
 			if(resultSet.next()) {
-				
+
 				String attachmentURL = resultSet.getString("attachmentURL");
 				AttachmentType attachmentTypeID = AttachmentType.getAttachmentType(resultSet.getInt("attachmentTypeID"));
-				
+
 				attachment = new Attachment(attachmentID, attachmentURL, attachmentTypeID);
 			}
-			
+
 			conn.close();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return attachment;
-		
+
 	}
-	
+
 	public int createAttachment(String attachmentURL, int attachmentTypeID) {
-		
+
 		int attachmentID = -1;
-		
+
 		try {
-			
+
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 			ResultSet resultSet = null;
-			
+
 			String sql = "INSERT INTO `Attachment` (attachmentURL, attachmentTypeID) VALUES (?, ?)";
 			pst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			pst.setString(1, attachmentURL);
@@ -65,13 +67,23 @@ public class AttachmentService {
 			if (resultSet.next()) {
 				attachmentID = resultSet.getInt(1);
 			}
-			
-			
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return attachmentID;
 	}
-	
+
+	public String getFileName(Part part) {
+		for (String cd : part.getHeader("content-disposition").split(";")) {
+			if (cd.trim().startsWith("filename")) {
+				return cd.substring(cd.indexOf('=') + 1).trim()
+						.replace("\"", "");
+			}
+		}
+		return null;
+	}
+
 }
