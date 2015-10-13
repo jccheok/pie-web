9306afb8ff8edb22aff7312f49d7b4f0f1ba79ea
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-13 11:16:16 +0800

    createAttachment Method for AttachmentService

--- src/main/java/pie/services/AttachmentService.java
+++ src/main/java/pie/services/AttachmentService.java
@@ -3,6 +3,7 @@ package pie.services;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import pie.Attachment;
 import pie.AttachmentType;
@@ -42,6 +43,35 @@ public class AttachmentService {
 		
 		return attachment;
 		
+	}
+	
+	public int createAttachment(String attachmentURL, int attachmentTypeID) {
+		
+		int attachmentID = -1;
+		
+		try {
+			
+			Connection conn = DatabaseConnector.getConnection();
+			PreparedStatement pst = null;
+			ResultSet resultSet = null;
+			
+			String sql = "INSERT INTO `Attachment` (attachmentURL, attachmentTypeID) VALUES (?, ?)";
+			pst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			pst.setString(1, attachmentURL);
+			pst.setInt(2, attachmentTypeID);
+			pst.executeUpdate();
+
+			resultSet = pst.getGeneratedKeys();
+			if (resultSet.next()) {
+				attachmentID = resultSet.getInt(1);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return attachmentID;
 	}
 	
 }
