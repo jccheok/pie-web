6f19371986eda630af0624a85a70d70c910231b3
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-13 12:52:47 +0800

    Change filePath for GetAttachmentServlet

--- src/main/java/pie/servlets/GetAttachmentServlet.java
+++ src/main/java/pie/servlets/GetAttachmentServlet.java
@@ -31,7 +31,7 @@ public class GetAttachmentServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//ROUGH DRAFT
 		
-		String filePath = System.getenv("OPENSHIFT_DATA_DIR") + "/uploads";
+		String filePath = System.getenv("OPENSHIFT_DATA_DIR") + "/piedev";
 		File downloadFile = new File(filePath);
 		FileInputStream inStream = new FileInputStream(downloadFile);
 
