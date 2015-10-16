a2d5826318fba74e76e4cb30c6fef351d16258c0
jccheok <cheok.jia.chin@gmail.com>
2015-10-16 15:36:38 +0800

    Refactor PublishHomeworkResult

--- src/main/java/pie/constants/PublishHomeworkResult.java
+++ src/main/java/pie/constants/PublishHomeworkResult.java
@@ -2,14 +2,12 @@ package pie.constants;
 
 public enum PublishHomeworkResult {
 
-	SUCCESS("Homework successfully published!"), 
-	FAILED_TO_UPDATE_GROUP("Failed to send homework to group"), 
-	FAILED_TO_SEND_TO_MEMBERS("The homework failed to send to members"),
-	FAILED_DRAFT("Failed to set the homework to not draft");
+	SUCCESS("Homework successfully published!"), FAILED_TO_UPDATE_HOMEWORK("Failed to publish homework"), FAILED_DRAFT(
+			"Failed to set the homework to not draft");
 
 	private String defaultMessage;
-	
-	PublishHomeworkResult(String defaultMessage){
+
+	PublishHomeworkResult(String defaultMessage) {
 		this.defaultMessage = defaultMessage;
 	}
 
@@ -20,5 +18,5 @@ public enum PublishHomeworkResult {
 	public String getDefaultMessage() {
 		return defaultMessage;
 	}
-	
+
 }
