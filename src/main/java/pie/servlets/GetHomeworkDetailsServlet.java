b1720da268a43bdabf4d4526f8246ebfd0a4d5c7
jccheok <cheok.jia.chin@gmail.com>
2015-10-19 17:34:12 +0800

    Initial commit of GetHomeworkDetailsServlet

---
+++ src/main/java/pie/servlets/GetHomeworkDetailsServlet.java
@@ -0,0 +1,62 @@
+package pie.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import pie.Homework;
+import pie.services.HomeworkService;
+import pie.utilities.Utilities;
+
+@Singleton
+public class GetHomeworkDetailsServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8818437379452661718L;
+	private HomeworkService homeworkService;
+
+	@Inject
+	public GetHomeworkDetailsServlet(HomeworkService homeworkService) {
+		this.homeworkService = homeworkService;
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int homeworkID = 0;
+
+		try {
+
+			Map<String, String> requestParams = Utilities.getParameters(request, "homeworkID");
+			homeworkID = Integer.parseInt(requestParams.get("homeworkID"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Homework homework = homeworkService.getHomework(homeworkID);
+
+		JSONObject responseObject = new JSONObject();
+
+		responseObject.put("dateCreated", homework.getHomeworkDateCreated());
+		responseObject.put("authorBy", homework.getHomeworkAuthor().getUserFullName());
+		responseObject.put("description", homework.getHomeworkDescription());
+		responseObject.put("level", homework.getHomeworkLevel());
+		responseObject.put("title", homework.getHomeworkTitle());
+		responseObject.put("subject", homework.getHomeworkSubject());
+
+		PrintWriter out = response.getWriter();
+		out.write(responseObject.toString());
+	}
+}
