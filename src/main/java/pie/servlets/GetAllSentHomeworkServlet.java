e51882b3d549dc533107480d6eb5364a602d6e55
markteo <mark-543210@hotmail.com>
2015-10-19 11:14:56 +0800

    Fix spelling error in GetAllSentHomeworkServlet

--- src/main/java/pie/servlets/GetAllSentHomeworkServlet.java
+++ src/main/java/pie/servlets/GetAllSentHomeworkServlet.java
@@ -59,7 +59,7 @@ public class GetAllSentHomeworkServlet extends HttpServlet{
 				homeworkObject.put("homeworkTitle", homework.getHomework().getHomeworkTitle());
 				homeworkObject.put("homeworkDescription", homework.getHomework().getHomeworkDescription());
 				homeworkObject.put("publisherName", homework.getPublisher().getUserFullName());
-				homeworkObject.put("publishedDaate", homework.getPublishDate());
+				homeworkObject.put("publishedDate", homework.getPublishDate());
 				
 				sentHomeworkList.put(homeworkObject);
 			}
