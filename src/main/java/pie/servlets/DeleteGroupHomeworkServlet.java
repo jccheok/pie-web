e921cfa795129b9b6600c06ba0cfca796d707d4f
markteo <mark-543210@hotmail.com>
2015-10-19 12:10:00 +0800

    Initial commit of DeleteGroupHomeworkServlet

---
+++ src/main/java/pie/servlets/DeleteGroupHomeworkServlet.java
@@ -0,0 +1,61 @@
+package pie.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import pie.services.GroupHomeworkService;
+import pie.utilities.Utilities;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class DeleteGroupHomeworkServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 4001648799535140886L;
+	
+	GroupHomeworkService groupHomeworkService;
+	
+	@Inject
+	public DeleteGroupHomeworkServlet(GroupHomeworkService groupHomeworkService) {
+		this.groupHomeworkService = groupHomeworkService;
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int groupHomeworkID = 0;
+		
+		try {
+
+			Map<String, String> requestParameters = Utilities.getParameters(request, "groupHomeworkID");
+
+			groupHomeworkID = Integer.parseInt(requestParameters.get("groupHomeworkID"));
+
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+			return;
+		}
+
+		boolean deleteResult = groupHomeworkService.deleteSentHomework(groupHomeworkID);
+		
+		JSONObject responseObject = new JSONObject();
+		if(deleteResult){
+			responseObject.put("result", "Success");
+			responseObject.put("message", "Successfully deleted the message");
+		}else{
+			responseObject.put("result","Failure");
+			responseObject.put("message", "Failed to delete message");
+		}
+		
+		PrintWriter out = response.getWriter();
+		out.write(responseObject.toString());
+	}
+}
