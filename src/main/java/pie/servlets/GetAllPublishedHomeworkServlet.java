5737d49abc6218cfd309545ec96c11515fdad4ad
jccheok <cheok.jia.chin@gmail.com>
2015-10-19 17:15:47 +0800

    Initial commit GetAllPublishedHomeworkServlet

---
+++ src/main/java/pie/servlets/GetAllPublishedHomeworkServlet.java
@@ -0,0 +1,68 @@
+package pie.servlets;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONObject;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import pie.Homework;
+import pie.services.GroupService;
+import pie.services.HomeworkService;
+import pie.services.StaffService;
+import pie.utilities.Utilities;
+
+@Singleton
+public class GetAllPublishedHomeworkServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5438895086826432496L;
+	private HomeworkService homeworkService;
+	private GroupService groupService;
+	private StaffService staffService;
+
+	@Inject
+	public GetAllPublishedHomeworkServlet(HomeworkService homeworkService, GroupService groupService,
+			StaffService staffService) {
+		this.homeworkService = homeworkService;
+		this.groupService = groupService;
+		this.staffService = staffService;
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JSONObject responseObj = new JSONObject();
+		int staffID = 0;
+		try {
+			Map<String, String> requestParams = Utilities.getParameters(request, "staffID");
+			staffID = Integer.parseInt(requestParams.get("staffID"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Homework[] publishedHomework = homeworkService.getAllPublishedHomework(staffID);
+
+		JSONArray listHomework = new JSONArray();
+
+		for (Homework homework : publishedHomework) {
+			JSONObject jsonHomework = new JSONObject();
+			jsonHomework.put("title", homework.getHomeworkTitle());
+			jsonHomework.put("subject", homework.getHomeworkSubject());
+			jsonHomework.put("description", homework.getHomeworkDescription());
+			listHomework.put(jsonHomework);
+		}
+
+		responseObj.put("listPublishedHomework", listHomework);
+	}
+}
