798cd381838b437fe075ffb079d31fd1d49d01f9
markteo <mark-543210@hotmail.com>
2015-10-19 16:17:57 +0800

    Initial commit of GradeHomeworkServlet

--- src/main/java/pie/servlets/GradeHomeworkServlet.java
+++ src/main/java/pie/servlets/GradeHomeworkServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,10 +17,13 @@ import pie.services.UserHomeworkService;
 import pie.utilities.Utilities;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
-public class GradeHomeworkServlet {
+@Singleton
+public class GradeHomeworkServlet extends HttpServlet {
 
-
+	private static final long serialVersionUID = -1476362889809343791L;
+	
 	UserHomeworkService userHomeworkService;
 	
 	@Inject
@@ -46,20 +50,26 @@ public class GradeHomeworkServlet {
 		JSONObject requestObject = new JSONObject(rawStudentList);
 		JSONArray studentList = requestObject.getJSONArray("studentList");
 
+		
+		JSONObject responseObject = new JSONObject();
 		for (int index = 0; index < studentList.length(); index++) {
 
 			JSONObject student = studentList.getJSONObject(index);
 
 			int studentID = student.getInt("studentID");
 			int userHomeworkID = student.getInt("userHomeworkID");
+			String homeworkGrade = student.getString("homeworkGrade");
 
-			userHomeworkService.setHomeworkGrade(userHomeworkID, studentID, homeworkGrade)
+			if(!userHomeworkService.setHomeworkGrade(userHomeworkID, studentID, homeworkGrade)){
+				responseObject.put("result", GenericResult.FAILED.toString());
+				responseObject.put("message", "Failed to grade homework");
+				break;
+
+			}else{
+				responseObject.put("result", GenericResult.SUCCESS.toString());
+				responseObject.put("message", "Successfully graded homework");
+			}
 		}
-
-		JSONObject responseObject = new JSONObject();
-		responseObject.put("result", GenericResult.SUCCESS.toString());
-		responseObject.put("message", "Successfully marked homework.");
-
 		PrintWriter out = response.getWriter();
 		out.write(responseObject.toString());
 	}
