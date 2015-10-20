02b43c2a273be89ca9c7132e936feaa160b2d32d
markteo <mark-543210@hotmail.com>
2015-10-20 12:38:29 +0800

    Format UserHomeworkService file

--- src/main/java/pie/services/UserHomeworkService.java
+++ src/main/java/pie/services/UserHomeworkService.java
@@ -6,18 +6,20 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Date;
 
+import pie.GroupHomework;
 import pie.Homework;
+import pie.Staff;
 import pie.User;
 import pie.UserHomework;
 import pie.utilities.DatabaseConnector;
 
 public class UserHomeworkService {
 
-	public UserHomework getUserHomework(int userHomeworkID){
+	public UserHomework getUserHomework(int userHomeworkID) {
 		UserHomework userHomework = null;
-		
-		try{
-			
+
+		try {
+
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 			ResultSet resultSet = null;
@@ -35,30 +37,30 @@ public class UserHomeworkService {
 
 				User user = userService.getUser(resultSet.getInt("userID"));
 				Homework homework = homeworkService.getHomework(resultSet.getInt("homeworkID"));
-				
+
 				Date submissionDate = new Date(resultSet.getDate("submissionDate").getTime());
 				Date dateArchived = new Date(resultSet.getDate("dateArchived").getTime());
 				Date dateRead = new Date(resultSet.getDate("dateRead").getTime());
 				boolean isSubmitted = resultSet.getInt("isSubmitted") == 1 ? true : false;
 				boolean isArchived = resultSet.getInt("isArchived") == 1 ? true : false;
 				boolean isDeleted = resultSet.getInt("isDeleted") == 1 ? true : false;
-				boolean isRead = resultSet.getInt("isRead") == 1 ? true:false;
-				boolean isMarked = resultSet.getInt("isMarked") == 1 ? true: false;
+				boolean isRead = resultSet.getInt("isRead") == 1 ? true : false;
+				boolean isMarked = resultSet.getInt("isMarked") == 1 ? true : false;
 				String grade = resultSet.getString("grade");
 
-				userHomework = new UserHomework(userHomeworkID, homework, user, isRead, isSubmitted, submissionDate, isArchived, dateArchived, dateRead, grade, isMarked, isDeleted);
+				userHomework = new UserHomework(userHomeworkID, homework, user, isRead, isSubmitted, submissionDate,
+						isArchived, dateArchived, dateRead, grade, isMarked, isDeleted);
 			}
 
 			conn.close();
-	
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return userHomework;
 	}
-	
+
 	public boolean sendHomework(int studentID, int homeworkID) {
 
 		boolean sendResult = false;
@@ -84,13 +86,12 @@ public class UserHomeworkService {
 
 		return sendResult;
 	}
-	
-	
-	public boolean deleteHomework(int userHomeworkID){
+
+	public boolean deleteHomework(int userHomeworkID) {
 		boolean deleteResult = false;
-		
-		try{
-			
+
+		try {
+
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 
@@ -98,26 +99,25 @@ public class UserHomeworkService {
 			pst = conn.prepareStatement(sql);
 			pst.setInt(1, 1);
 			pst.setInt(2, userHomeworkID);
-			
+
 			pst.executeUpdate();
-			
+
 			deleteResult = true;
-			
+
 			conn.close();
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return deleteResult;
 	}
-	
-	public boolean markHomework(int userHomeworkID, int userID){
+
+	public boolean markHomework(int userHomeworkID, int userID) {
 		boolean markedResult = false;
-		
-		try{
-			
+
+		try {
+
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 
@@ -126,25 +126,25 @@ public class UserHomeworkService {
 			pst.setInt(1, 1);
 			pst.setInt(2, userID);
 			pst.setInt(3, userHomeworkID);
-			
+
 			pst.executeUpdate();
-			
+
 			markedResult = true;
-			
+
 			conn.close();
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return markedResult;
 	}
-	
-	public boolean setHomeworkGrade(int userHomeworkID, int userID, String homeworkGrade){
+
+	public boolean setHomeworkGrade(int userHomeworkID, int userID, String homeworkGrade) {
 		boolean gradeResult = false;
-		
-		try{
-			
+
+		try {
+
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 
@@ -154,69 +154,79 @@ public class UserHomeworkService {
 			pst.setString(2, homeworkGrade);
 			pst.setInt(3, userID);
 			pst.setInt(4, userHomeworkID);
-			
+
 			pst.executeUpdate();
-			
+
 			gradeResult = true;
-			
+
 			conn.close();
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return gradeResult;
 	}
-	
-	public UserHomework[] getUserHomeworkRecipients(int homeworkID, int groupHomeworkID, int publisherID){
+
+	public UserHomework[] getUserHomeworkRecipients(int homeworkID, int groupHomeworkID, int publisherID) {
 		UserHomework[] userHomeworkRecipients = {};
-		
-		try{
+
+		try {
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 			ResultSet resultSet = null;
-			
+
 			String sql = "SELECT userHomeworkID FROM `Homework`, `GroupHomework`, `UserHomework` WHERE `Homework`.homeworkID = `GroupHomework`.homeworkID AND `UserHomework`.homeworkID = `Homework`.homeworkID "
 					+ "AND `GroupHomework`.groupHomeworkID = ? AND `GroupHomework`.publisherID = ? AND `UserHomework`.homeworkID = ?";
 			pst = conn.prepareStatement(sql);
 			pst.setInt(1, groupHomeworkID);
 			pst.setInt(2, publisherID);
 			pst.setInt(3, homeworkID);
-			
+
 			resultSet = pst.executeQuery();
-			
+
 			ArrayList<UserHomework> tempUserHomeworkList = new ArrayList<UserHomework>();
-			while(resultSet.next()){
+			while (resultSet.next()) {
 				tempUserHomeworkList.add(getUserHomework(resultSet.getInt("userHomeworkID")));
 			}
-			
+
 			userHomeworkRecipients = tempUserHomeworkList.toArray(userHomeworkRecipients);
-			
+
 			conn.close();
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return userHomeworkRecipients;
 	}
-	
-	public boolean readHomework(int userHomeworkID){
+
+	public boolean readHomework(int userHomeworkID) {
 		boolean readResult = false;
 
-		try{
-			
+		try {
+
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 
-			String sql = "UPDATE `UserHomework` SET isRead = ? WHERE userHomeworkID = ?";
+			String sql = "UPDATE `UserHomework` SET isRead = ?, dateRead = NOW() WHERE userHomeworkID = ?";
 			pst = conn.prepareStatement(sql);
 			pst.setInt(1, 1);
 			pst.setInt(2, userHomeworkID);
-			
+
 			pst.executeUpdate();
-			
+
 			readResult = true;
+
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return readResult;
+	}
+
 	public boolean archiveHomework(int userHomeworkID) {
 		boolean archiveResult = false;
 
@@ -328,6 +338,8 @@ public class UserHomeworkService {
 		}
 		
 		return publisher;
+	}
+	
 	public GroupHomework getGroupHomework (int userHomeworkID, int publisherID){
 		GroupHomework groupHomework = null;
 		
