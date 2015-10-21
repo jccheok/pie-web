c0b1bed2f9daf9626f6d41afc0a0e546ef147675
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-21 10:59:44 +0800

    Update UserNoteService
    Update isRead method with the correct query.

--- src/main/java/pie/services/UserNoteService.java
+++ src/main/java/pie/services/UserNoteService.java
@@ -89,7 +89,7 @@ public class UserNoteService {
 		
 	}
 	
-	public boolean isRead(int userNoteID) {
+	public boolean isRead(int noteID, int userID) {
 		
 		boolean isRead = false;
 		
@@ -98,9 +98,10 @@ public class UserNoteService {
 			Connection conn = DatabaseConnector.getConnection();
 			PreparedStatement pst = null;
 			
-			String sql = "UPDATE `UserNote` SET isRead = 1, SET dateRead = NOW() WHERE userNoteID = ?";
+			String sql = "UPDATE `UserNote` SET isRead = 1, SET dateRead = NOW() WHERE noteID = ? and userID = ?";
 			pst = conn.prepareStatement(sql);
-			pst.setInt(1, userNoteID);
+			pst.setInt(1, noteID);
+			pst.setInt(2, userID);
 			
 			pst.executeUpdate();
 			isRead = true;
