7d9a07632a1d8ccdce2922cb10f43dcc919b0945
markteo <mark-543210@hotmail.com>
2015-10-21 15:24:43 +0800

    Change the import for ViewAllGroupMembersServlet to GetAllRecipientsServlet

--- src/main/java/pie/utilities/GuiceServletConfig.java
+++ src/main/java/pie/utilities/GuiceServletConfig.java
@@ -67,7 +67,7 @@ import pie.servlets.UpdateUserAccountDetailsServlet;
 import pie.servlets.UploadHomeworkAttachmentServlet;
 import pie.servlets.UploadNoteAttachmentServlet;
 import pie.servlets.VerifyUserServlet;
-import pie.servlets.ViewAllGroupMembersServlet;
+import pie.servlets.GetAllRecipientsServlet;
 import pie.servlets.ViewAllSchoolsServlet;
 import pie.servlets.ViewCitiesServlet;
 import pie.servlets.ViewGroupDetailsServlet;
@@ -130,7 +130,7 @@ public class GuiceServletConfig extends GuiceServletContextListener {
 				serve("*/servlets/secured/staff/group/owner/enliststudents").with(EnlistStudentsToGroupServlet.class);
 				serve("*/servlets/secured/staff/group/owner/transfergroupownership").with(TransferGroupOwnershipServlet.class);
 				serve("*/servlets/secured/staff/group/owner/deactivategroup").with(DeactivateGroupServlet.class);
-				serve("*/servlets/secured/staff/group/member/allgroupmembers").with(ViewAllGroupMembersServlet.class);				
+				serve("*/servlets/secured/staff/group/member/allrecipients").with(GetAllRecipientsServlet.class);				
 				
 				serve("*/servlets/secured/staff/group/sendnote").with(SendNoteServlet.class);
 				serve("*/servlets/secured/staff/group/savenoteasdraft").with(SaveNoteAsDraftServlet.class);
