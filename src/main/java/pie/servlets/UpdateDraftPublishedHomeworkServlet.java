16f34a60130b70acc2bb05658be2d661bb451699
markteo <mark-543210@hotmail.com>
2015-10-21 12:32:16 +0800

    Remove actualMarkingCompletionDate in UpdateDraftPublishedHomeworkServlet

--- src/main/java/pie/servlets/UpdateDraftPublishedHomeworkServlet.java
+++ src/main/java/pie/servlets/UpdateDraftPublishedHomeworkServlet.java
@@ -78,14 +78,14 @@ public class UpdateDraftPublishedHomeworkServlet extends HttpServlet {
 
 			Map<String, String> requestParameters = Utilities.getParameters(request, "groupID", "homeworkID",
 					"staffID",
-					"markingEffort", "actualMarkingCompletionDate", "targetMarkingCompletionDate", "dueDate",
+					"markingEffort", "targetMarkingCompletionDate", "dueDate",
 					"isGraded", "groupHomeworkID");
 			groupHomeworkID = Integer.parseInt(requestParameters.get("groupHomeworkID"));
 			publisher = staffService.getStaff(Integer.parseInt(requestParameters.get("staffID")));
 			group = groupService.getGroup(Integer.parseInt(requestParameters.get("groupID")));
 			homework = homeworkService.getHomework(Integer.parseInt(requestParameters.get("homeworkID")));
 			markingEffort = Integer.parseInt(requestParameters.get("markingEffort"));
-			actualMarkingCompletionDate = dateFormat.parse(requestParameters.get("actualMarkingCompletionDate"));
+			actualMarkingCompletionDate = dateFormat.parse("1000-01-01");
 			targetMarkingCompletionDate = dateFormat.parse(requestParameters.get("targetMarkingCompletionDate"));
 			dueDate = dateFormat.parse(requestParameters.get("dueDate"));
 			isGraded = Integer.parseInt(requestParameters.get("isGraded")) == 1 ? true : false;
