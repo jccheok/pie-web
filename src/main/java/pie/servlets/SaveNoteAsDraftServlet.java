cecfb262a321acc13b08161da7494848440b9ebc
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-22 16:07:11 +0800

    Update SaveNoteAsDraftServlet
    Updated servlet to the latest with Apache Common FileUpload

--- src/main/java/pie/servlets/SaveNoteAsDraftServlet.java
+++ src/main/java/pie/servlets/SaveNoteAsDraftServlet.java
@@ -1,19 +1,23 @@
 package pie.servlets;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.json.JSONObject;
 
 import pie.services.NoteAttachmentService;
 import pie.services.NoteService;
-import pie.utilities.Utilities;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -21,7 +25,9 @@ import com.google.inject.Singleton;
 @Singleton
 public class SaveNoteAsDraftServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 9157470052062586665L;
+	private static final long serialVersionUID = -9205572661568286854L;
+	private static final int maxRequestSize = 1024*1024*10;
+	private static final int memorySize = 1024*1024*3;
 
 	NoteService noteService;
 	NoteAttachmentService noteAttachmentService;
@@ -36,46 +42,103 @@ public class SaveNoteAsDraftServlet extends HttpServlet {
 
 		int noteID = 0;
 		int staffID = 0;
+		int groupID = 0;
 		int responseQuestionID = 0;
 		int noteAttachmentID = 1;
+		boolean fileDetected = false;
 		String noteTitle = null;
 		String noteDescription = null;
+		String noteAttachmentURL = null;
+		FileItem fileUpload = null;
+
+		JSONObject responseObject = new JSONObject();
+		PrintWriter out = response.getWriter();
+
+		if(noteAttachmentService.checkIfNoteFolderExist()) {
+			responseObject.put("folderResult", "Note Folder exist");
+		} else {
+			responseObject.put("folderResult", "Note Folder did not exist but was created during the process");
+		}
+
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setSizeThreshold(memorySize);
+		factory.setRepository(new File(System.getenv("OPENSHIFT_TMP_DIR")));
+
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		upload.setSizeMax(maxRequestSize);
 
 		try {
 
-			Map<String, String> requestParameters = Utilities.getParameters(request, "staffID", "responseQuestionID", "noteAttachmentID", "noteTitle", "noteDescription");
+			List<FileItem> items = upload.parseRequest(request);
 
-			staffID = Integer.parseInt(requestParameters.get("staffID"));
-			responseQuestionID = Integer.parseInt(requestParameters.get("responseQuestionID"));
-			noteAttachmentID = Integer.parseInt(requestParameters.get("noteAttachmentID"));
-			noteTitle = requestParameters.get("noteTitle");
-			noteDescription = requestParameters.get("noteDescription");
+			if(items != null && items.size() > 0) {
 
-		} catch (Exception e) {
+				Iterator<FileItem> iter = items.iterator();
+				while (iter.hasNext()) {
+					FileItem item = iter.next();
 
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
-			return;
-		}
+					if (!item.isFormField() && item.getSize() > 0) {
 
-		noteID = noteService.createNote(staffID, responseQuestionID, noteTitle, noteDescription);
-		JSONObject responseObject = new JSONObject();
+						fileDetected = true;
+						fileUpload = item;
 
-		if (noteID != -1) {
-			
-			if(noteAttachmentID != 1) {
-				noteAttachmentService.updateNoteAttachmentID(noteAttachmentID, noteID);
+						noteAttachmentURL = new File(item.getName()).getName();
+
+					} else {
+
+						if(item.getFieldName().equalsIgnoreCase("staffID")) {
+							staffID = Integer.parseInt(item.getString());
+							responseObject.put("staffID", staffID);
+						} else if(item.getFieldName().equalsIgnoreCase("groupID")) {
+							groupID = Integer.parseInt(item.getString());
+							responseObject.put("groupID", groupID);
+						} else if(item.getFieldName().equalsIgnoreCase("responseQuestionID")) {
+							responseQuestionID = Integer.parseInt(item.getString());
+							responseObject.put("responseQuestion", responseQuestionID);
+						} else if(item.getFieldName().equalsIgnoreCase("noteTitle")) {
+							noteTitle = item.getString();
+							responseObject.put("noteTitle", noteTitle);
+						} else if(item.getFieldName().equalsIgnoreCase("noteDescription")) {
+							noteDescription = item.getString();
+							responseObject.put("noteDescription", noteDescription);
+						}
+					} 
+				}
+
+			} else {
+				responseObject.put("uploadResult", "No item found");
 			}
 			
-			responseObject.put("result", "SUCCESS");
-			responseObject.put("message", "Note is successfully created and saved as draft");
-		} else {
-			responseObject.put("result", "FAILED");
-			responseObject.put("message", "Note is not created!");
+			if(staffID != 0 && responseQuestionID != 0) {
+
+				noteID = noteService.createNote(staffID, responseQuestionID, noteTitle, noteDescription);
+
+				if(fileDetected) {
+
+					noteAttachmentID = noteAttachmentService.createNoteAttachment(noteAttachmentURL, noteID);
+					noteAttachmentURL = noteAttachmentService.updateNoteAttachmentName(noteAttachmentID, noteAttachmentURL);
+
+					File storeFile = new File(noteAttachmentService.getNoteAttachmentDIR(noteAttachmentURL));
+
+					fileUpload.write(storeFile);
+
+					responseObject.put("fileResult", "SUCCESS");
+					responseObject.put("noteAttachmentID", noteAttachmentID);
+					responseObject.put("noteAttachmentURL", noteAttachmentURL);
+
+				} else {
+					responseObject.put("fileResult", "FAILED - NO FILE UPLOADED");
+				}
+
+			} else {
+				responseObject.put("noteResult", "Creation of note failed");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
-		PrintWriter out = response.getWriter();
 		out.write(responseObject.toString());
 
 	}
-
 }
