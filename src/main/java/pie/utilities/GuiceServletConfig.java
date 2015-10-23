f112a74dbe657daf9ddbb612e71cd13e4b86991f
markteo <mark-543210@hotmail.com>
2015-10-23 11:50:47 +0800

    Refactor Guice config

--- src/main/java/pie/utilities/GuiceServletConfig.java
+++ src/main/java/pie/utilities/GuiceServletConfig.java
@@ -24,7 +24,7 @@ import pie.servlets.GetAllSecurityQuestionsServlet;
 import pie.servlets.GetAllSentHomeworkServlet;
 import pie.servlets.GetAllSentNotesServlet;
 import pie.servlets.GetAllSubjectsServlet;
-import pie.servlets.GetAllUserHomeworkServlet;
+import pie.servlets.GetAllParentHomeworkServlet;
 import pie.servlets.GetHomeworkDetailsServlet;
 import pie.servlets.GetHomeworkRecipientsServlet;
 import pie.servlets.GetNoteDetailsServlet;
@@ -176,7 +176,7 @@ public class GuiceServletConfig extends GuiceServletContextListener {
 				serve("*/servlets/secured/student/deletehomework").with(DeleteUserHomeworkServlet.class); //tested
 				serve("*/servlets/secured/student/archivehomework").with(ArchiveHomeworkServlet.class);//tested
 				serve("*/servlets/secured/student/readhomework").with(ReadHomeworkServlet.class);//tested
-				serve("*/servlets/secured/student/allrecievedhomework").with(GetAllUserHomeworkServlet.class);//tested
+				serve("*/servlets/secured/parent/allparentrecievedhomework").with(GetAllParentHomeworkServlet.class);//tested
 
 				serve("*/servlets/secured/staff/group/uploadhomeworkattachment").with(UploadHomeworkAttachmentServlet.class);
 				serve("*/servlets/secured/staff/group/downloadhomeworkattachment").with(DownloadHomeworkAttachmentServlet.class);
