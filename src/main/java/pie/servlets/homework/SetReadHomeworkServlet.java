432c02b16186c6921847b8fc5ad700648802163f
markteo <mark-543210@hotmail.com>
2015-10-26 14:48:31 +0800

    Rename ReadHomeworkServlet to SetReadHomeworkServlet

--- src/main/java/pie/servlets/homework/SetReadHomeworkServlet.java
+++ src/main/java/pie/servlets/homework/SetReadHomeworkServlet.java
@@ -18,33 +18,35 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Singleton
-public class ReadHomeworkServlet extends HttpServlet{
+public class SetReadHomeworkServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1143370964920298110L;
 
 	UserHomeworkService userHomeworkService;
 	
 	@Inject
-	public ReadHomeworkServlet(UserHomeworkService userHomeworkService) {
+	public SetReadHomeworkServlet(UserHomeworkService userHomeworkService) {
 		this.userHomeworkService = userHomeworkService;
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		int userHomeworkID = 0;
+		boolean isRead = false;
 		
 		try {
 
-			Map<String, String> requestParameters = Utilities.getParameters(request, "userHomeworkID");
+			Map<String, String> requestParameters = Utilities.getParameters(request, "userHomeworkID", "isRead");
 
 			userHomeworkID = Integer.parseInt(requestParameters.get("userHomeworkID"));
+			isRead = Integer.parseInt(requestParameters.get("isRead")) == 1;
 
 		} catch (Exception e) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
 			return;
 		}
 
-		boolean readResult = userHomeworkService.readHomework(userHomeworkID);
+		boolean readResult = userHomeworkService.readHomework(userHomeworkID, isRead);
 		JSONObject responseObject = new JSONObject();
 		
 		if(readResult){
