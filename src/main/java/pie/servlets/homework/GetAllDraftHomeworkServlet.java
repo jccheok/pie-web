65c47005e787e781e48da2127aeed918cd7a0897
jccheok <cheok.jia.chin@gmail.com>
2015-10-28 12:40:51 +0800

    Remove homeworkID from jsonHomework in GetAllDraftHomeworkServlet

--- src/main/java/pie/servlets/homework/GetAllDraftHomeworkServlet.java
+++ src/main/java/pie/servlets/homework/GetAllDraftHomeworkServlet.java
@@ -59,7 +59,6 @@ public class GetAllDraftHomeworkServlet extends HttpServlet {
 		if (listHomework != null) {
 			for (Homework homework : listHomework) {
 				JSONObject jsonHomework = new JSONObject();
-				jsonHomework.put("homeworkID", homework.getHomeworkID());
 				jsonHomework.put("staffID", homework.getHomeworkAuthor().getUserID());
 				jsonHomework.put("title", homework.getHomeworkTitle());
 				jsonHomework.put("subject", homework.getHomeworkSubject());
