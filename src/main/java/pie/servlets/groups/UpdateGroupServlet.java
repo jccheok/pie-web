4a9a4a8662a5e6b60ffd57d2215339cc6a4bd0d4
markteo <mark-543210@hotmail.com>
2015-10-28 13:22:36 +0800

    Refactor UpdateGroupServlet

--- src/main/java/pie/servlets/groups/UpdateGroupServlet.java
+++ src/main/java/pie/servlets/groups/UpdateGroupServlet.java
@@ -37,19 +37,19 @@ public class UpdateGroupServlet extends HttpServlet{
 		String groupName = null;
 		String groupDescription = null;
 		int groupMaxDailyHomeworkMinutes = 0;
-		boolean groupIsOpen = false;
+		boolean groupIsOpen = true;
 		String groupCode = null;
 		Date expiryDate = null;
 
 		try {
 
 			Map<String, String> requestParameters = Utilities.getParameters(request, "groupID", "groupName",
-					"groupDescription", "groupMaxDailyHomeworkMinutes", "groupIsOpen", "groupCode", "expiryDate");
+					"groupDescription", "groupMaxDailyHomeworkMinutes", "groupCode", "expiryDate");
 			groupID = Integer.parseInt(requestParameters.get("groupID"));
 			groupName = requestParameters.get("groupName");
 			groupDescription = requestParameters.get("groupDescription");
 			groupMaxDailyHomeworkMinutes = Integer.parseInt(requestParameters.get("groupMaxDailyHomeworkMinutes"));
-			groupIsOpen = Integer.parseInt(requestParameters.get("groupIsOpen")) == 1;
+			//groupIsOpen = Integer.parseInt(requestParameters.get("groupIsOpen")) == 1;
 			groupCode = requestParameters.get("groupCode");
 			expiryDate = Utilities.parseClientDate(requestParameters.get("expiryDate"));
 
