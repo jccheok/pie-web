6aff53aea10a941bef8d4a085931327ce93b9be6
markteo <mark-543210@hotmail.com>
2015-10-29 11:47:01 +0800

    Refactor ViewParentChildrenServlet

--- src/main/java/pie/servlets/parent/ViewParentChildrenServlet.java
+++ src/main/java/pie/servlets/parent/ViewParentChildrenServlet.java
@@ -32,23 +32,24 @@ import com.google.inject.Singleton;
 public class ViewParentChildrenServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 11940297912398677L;
-	
+
 	ParentStudentService parentStudentService;
 	StudentService studentService;
 	StudentGroupService studentGroupService;
 	UserHomeworkService userHomeworkService;
 	GroupHomeworkService groupHomeworkService;
-	
+
 	@Inject
-	public ViewParentChildrenServlet(ParentStudentService parentStudentService, StudentService studentService, 
-			StudentGroupService studentGroupService, UserHomeworkService userHomeworkService, GroupHomeworkService groupHomeworkService) {
+	public ViewParentChildrenServlet(ParentStudentService parentStudentService, StudentService studentService,
+			StudentGroupService studentGroupService, UserHomeworkService userHomeworkService,
+			GroupHomeworkService groupHomeworkService) {
 		this.parentStudentService = parentStudentService;
 		this.studentService = studentService;
 		this.studentGroupService = studentGroupService;
 		this.userHomeworkService = userHomeworkService;
 		this.groupHomeworkService = groupHomeworkService;
 	}
-	
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		int parentID = 0;
@@ -65,20 +66,20 @@ public class ViewParentChildrenServlet extends HttpServlet {
 		}
 
 		JSONObject responseObject = new JSONObject();
-		
+
 		ArrayList<Integer> groupHomeworkIDList = new ArrayList<Integer>();
 		UserHomework[] parentHomeworkList = userHomeworkService.getAllUserHomework(parentID);
-		
-		for(UserHomework parentHomework : parentHomeworkList){
+
+		for (UserHomework parentHomework : parentHomeworkList) {
 			groupHomeworkIDList.add(parentHomework.getGroupHomeworkID());
 		}
-		
+
 		JSONArray childrenList = new JSONArray();
 		for (Student student : parentStudentService.getChildren(parentID)) {
-			
+
 			int studentID = student.getUserID();
 			Parent mainParent = parentStudentService.getMainParent(studentID);
-			
+
 			JSONObject studentDetails = new JSONObject();
 			studentDetails.put("studentID", studentID);
 			studentDetails.put("studentFirstName", student.getUserFirstName());
@@ -87,41 +88,48 @@ public class ViewParentChildrenServlet extends HttpServlet {
 			studentDetails.put("studentSchoolID", student.getSchool().getSchoolID());
 			studentDetails.put("mainParentID", mainParent.getUserID());
 			studentDetails.put("mainParentFirstName", mainParent.getUserFirstName());
-			
+
 			JSONArray studentGroupsList = new JSONArray();
 			for (Group joinedGroup : studentService.getJoinedGroups(studentID)) {
-				
+
 				int joinedGroupID = joinedGroup.getGroupID();
-				
+
 				JSONObject groupDetails = new JSONObject();
 				groupDetails.put("groupID", joinedGroupID);
 				groupDetails.put("groupName", joinedGroup.getGroupName());
 				groupDetails.put("groupTypeName", joinedGroup.getGroupType().toString());
-				groupDetails.put("studentGroupIndexNumber", studentService.getStudentGroupIndexNumber(joinedGroupID, studentID));
-				groupDetails.put("studentGroupJoinDateUnix", Utilities.toUnixSeconds(studentGroupService.getStudentGroupJoinDate(joinedGroupID, studentID)));
+				groupDetails.put("studentGroupIndexNumber",
+						studentService.getStudentGroupIndexNumber(joinedGroupID, studentID));
+				groupDetails.put("studentGroupJoinDateUnix",
+						Utilities.toUnixSeconds(studentGroupService.getStudentGroupJoinDate(joinedGroupID, studentID)));
 				studentGroupsList.put(groupDetails);
 			}
-			
+
 			JSONArray studentHomeworkList = new JSONArray();
-			for(UserHomework userHomework : userHomeworkService.getAllChildHomework(studentID)){
-				
+			for (UserHomework userHomework : userHomeworkService.getAllChildHomework(studentID)) {
+
 				JSONObject homeworkDetails = new JSONObject();
-				
+
 				int userHomeworkID = userHomework.getUserHomeworkID();
 				GroupHomework groupHomework = groupHomeworkService.getGroupHomework(userHomework.getGroupHomeworkID());
-				
-				for(int parentGroupHomeworkID : groupHomeworkIDList){
-					if(groupHomework.getGroupHomeworkID() == parentGroupHomeworkID){
-						homeworkDetails.put("parentHomeworkID", groupHomework.getGroupHomeworkID());
-						break;
+
+				for (int parentGroupHomeworkID : groupHomeworkIDList) {
+					if (groupHomework.getGroupHomeworkID() == parentGroupHomeworkID) {
+						for (UserHomework homework : parentHomeworkList) {
+							if (userHomework.getGroupHomeworkID() == parentGroupHomeworkID) {
+								homeworkDetails.put("parentHomeworkID", homework.getUserHomeworkID());
+								break;
+							}
+							break;
+						}
 					}
 				}
-				
+
 				homeworkDetails.put("homeworkTitle", userHomework.getHomework().getHomeworkTitle());
 				homeworkDetails.put("dueDate", Utilities.parseServletDateFormat(groupHomework.getDueDate()));
 				homeworkDetails.put("grade", userHomework.getGrade());
 				homeworkDetails.put("homeworkID", userHomework.getHomework().getHomeworkID());
-				
+
 				if (!userHomework.isSubmitted()) {
 					homeworkDetails.put("status", "Not Submitted");
 				} else if (!userHomework.isMarked()) {
@@ -131,9 +139,9 @@ public class ViewParentChildrenServlet extends HttpServlet {
 				} else {
 					homeworkDetails.put("status", "Graded");
 				}
-				
+
 				studentHomeworkList.put(homeworkDetails);
-				
+
 			}
 			studentDetails.put("studentJoinedGroups", studentGroupsList);
 			studentDetails.put("studentHomework", studentHomeworkList);
