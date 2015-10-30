7bd073a66b5d53dc78852033bb78b755aa899f08
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-30 14:04:59 +0800

    Initial Commit - UpdateDraftNoteServlet

---
+++ src/main/java/pie/servlets/notes/UpdateDraftNoteServlet.java
@@ -0,0 +1,126 @@
+package pie.servlets.notes;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.json.JSONObject;
+
+import pie.services.NoteAttachmentService;
+import pie.services.NoteService;
+import pie.utilities.Utilities;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class UpdateDraftNoteServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 4005417158171470069L;
+	private static final int maxRequestSize = 1024*1024*10;
+	private static final int memorySize = 1024*1024*3;
+
+	NoteService noteService;
+	NoteAttachmentService noteAttachmentService;
+
+	@Inject
+	public UpdateDraftNoteServlet(NoteService noteService, NoteAttachmentService noteAttachmentService) {
+		this.noteService = noteService;
+		this.noteAttachmentService = noteAttachmentService;
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int noteID = 0;
+		int responseQuestionID = 0;
+		int noteAttachmentID = 1;
+		boolean fileDetected = false;
+		String noteTitle = null;
+		String noteDescription = null;
+		String noteAttachmentURL = null;
+		FileItem fileUpload = null;
+
+		JSONObject responseObject = new JSONObject();
+		PrintWriter out = response.getWriter();
+
+		noteAttachmentService.checkIfNoteFolderExist();
+
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setSizeThreshold(memorySize);
+		factory.setRepository(new File(System.getenv("OPENSHIFT_TMP_DIR")));
+
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		upload.setSizeMax(maxRequestSize);
+
+		try {
+
+			List<FileItem> items = upload.parseRequest(request);
+
+			if(items != null && items.size() > 0) {
+
+				Iterator<FileItem> iter = items.iterator();
+				while (iter.hasNext()) {
+					FileItem item = iter.next();
+
+					if (!item.isFormField() && item.getSize() > 0) {
+
+						fileDetected = true;
+						fileUpload = item;
+
+						noteAttachmentURL = new File(item.getName()).getName();
+
+					} else {
+
+						if(item.getFieldName().equalsIgnoreCase("responseQuestionID")) {
+							responseQuestionID = Integer.parseInt(item.getString());
+						} else if(item.getFieldName().equalsIgnoreCase("noteTitle")) {
+							noteTitle = Utilities.cleanHtml(item.getString());
+						} else if(item.getFieldName().equalsIgnoreCase("noteDescription")) {
+							noteDescription = Utilities.cleanHtml(item.getString());
+						} else if(item.getFieldName().equalsIgnoreCase("noteID")) {
+							noteID = Integer.parseInt(item.getString());
+						}
+					} 
+				}
+			}
+
+			boolean isUpdated = noteService.updateNote(noteTitle, noteDescription, responseQuestionID, noteID);
+
+			if(isUpdated == true) {
+
+				if(fileDetected) {
+					noteAttachmentID = noteAttachmentService.createNoteAttachment(noteAttachmentURL, noteID);
+					noteAttachmentURL = noteAttachmentService.updateNoteAttachmentName(noteAttachmentID, noteAttachmentURL);
+
+					File storeFile = new File(noteAttachmentService.getNoteAttachmentDIR(noteAttachmentURL));
+					fileUpload.write(storeFile);
+				}
+
+				responseObject.put("result", "SUCCESS");
+				responseObject.put("message", "Note successfully updated");
+
+			} else {
+
+				responseObject.put("result", "FAILED");
+				responseObject.put("message", "Note failed to update");
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		out.write(responseObject.toString());
+
+	}
+}
