e7652be9ecaff587405b6b156484e5dfff2bf82e
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-30 16:37:35 +0800

    Add DeleteAttachment to DeleteDraftNoteServlet
    There might be attachment uploaded for draft.

--- src/main/java/pie/servlets/notes/DeleteDraftNoteServlet.java
+++ src/main/java/pie/servlets/notes/DeleteDraftNoteServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 
+import pie.services.NoteAttachmentService;
 import pie.services.NoteService;
 import pie.utilities.Utilities;
 
@@ -21,31 +22,36 @@ import com.google.inject.Singleton;
 public class DeleteDraftNoteServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1989917926801430744L;
-	
+
 	NoteService noteService;
-	
+	NoteAttachmentService noteAttachmentService;
+
 	@Inject
-	public DeleteDraftNoteServlet(NoteService noteService) {
+	public DeleteDraftNoteServlet(NoteService noteService, NoteAttachmentService noteAttachmentService) {
 		this.noteService = noteService;
+		this.noteAttachmentService = noteAttachmentService;
 	}
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		int noteID = 0;
-		
+
 		try {
-			
+
 			Map<String, String> requestParameters = Utilities.getParameters(request, "noteID");
 			noteID = Integer.parseInt(requestParameters.get("noteID"));
-			
+
 		} catch (Exception e) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
 			return;
 		}
-		
+
 		boolean isDeleted = noteService.deleteDraftNote(noteID);
 		JSONObject responseObject = new JSONObject();
-		
+
+		String noteAttachmentURL = noteAttachmentService.getNoteAttachmentURL(noteID);
+		noteAttachmentService.deleteNoteAttachment(noteAttachmentURL);
+
 		if(isDeleted == true) {
 			responseObject.put("result", "SUCCESS");
 			responseObject.put("message", "Draft note successfully deleted");
@@ -53,10 +59,10 @@ public class DeleteDraftNoteServlet extends HttpServlet {
 			responseObject.put("result", "FAILED");
 			responseObject.put("message", "Draft note is not deleted");
 		}
-		
+
 		PrintWriter out = response.getWriter();
 		out.write(responseObject.toString());
-		
+
 	}
-	
+
 }
