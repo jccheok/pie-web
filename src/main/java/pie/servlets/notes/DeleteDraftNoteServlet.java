7ad87e7fc6260821d7dbfc06a76f928149b0e323
hoyuanyi <yuanyi1122@hotmail.com>
2015-10-30 14:02:35 +0800

    Initial Commit - DeleteDraftNoteServlet

---
+++ src/main/java/pie/servlets/notes/DeleteDraftNoteServlet.java
@@ -0,0 +1,62 @@
+package pie.servlets.notes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import pie.services.NoteService;
+import pie.utilities.Utilities;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class DeleteDraftNoteServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1989917926801430744L;
+	
+	NoteService noteService;
+	
+	@Inject
+	public DeleteDraftNoteServlet(NoteService noteService) {
+		this.noteService = noteService;
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int noteID = 0;
+		
+		try {
+			
+			Map<String, String> requestParameters = Utilities.getParameters(request, "noteID");
+			noteID = Integer.parseInt(requestParameters.get("noteID"));
+			
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+			return;
+		}
+		
+		boolean isDeleted = noteService.deleteDraftNote(noteID);
+		JSONObject responseObject = new JSONObject();
+		
+		if(isDeleted == true) {
+			responseObject.put("result", "SUCCESS");
+			responseObject.put("message", "Draft note successfully deleted");
+		} else {
+			responseObject.put("result", "FAILED");
+			responseObject.put("message", "Draft note is not deleted");
+		}
+		
+		PrintWriter out = response.getWriter();
+		out.write(responseObject.toString());
+		
+	}
+	
+}
